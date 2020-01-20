e92326e7e85ab244e03bc5e13774b08f4ef00d85
pollen <pollen0102@163.com>
2020-01-20 11:08:41 +0800

    add springfolder

--- src/main/java/com/oracle/o2o/service/impl/AreaServiceImpl.java
+++ src/main/java/com/oracle/o2o/service/impl/AreaServiceImpl.java
@@ -1,9 +1,7 @@
 package com.oracle.o2o.service.impl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
 import com.oracle.o2o.dao.AreaDao;
 import com.oracle.o2o.entity.Area;
 import com.oracle.o2o.service.AreaService;
